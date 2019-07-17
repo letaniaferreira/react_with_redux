817266635784f22dec011e93d289a2c09266bc6d
Letania Ferreira <letania@rollbar.com>
2019-07-17 06:58:49 -0700

    Add SesonDisplay component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
@@ -20,7 +21,7 @@ class App extends React.Component {
         }
 
         if (this.state.lat && !this.state.errorMessage) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
         return <div>Loading</div>
