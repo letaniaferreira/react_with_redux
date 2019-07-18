4119f642430c47a92fb6b0c921542bf4fedc5dd8
Letania Ferreira <letania@rollbar.com>
2019-07-17 21:52:53 -0700

    Add helper function

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -15,8 +15,7 @@ class App extends React.Component {
         );
     }
 
-    render() {
-
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
         }
@@ -26,6 +25,15 @@ class App extends React.Component {
         }
 
         return <Spinner message="Please accept location request"  />;
+    }
+
+    render() {
+        return  (
+            <div  className="border red">
+                {this.renderContent()}
+            </div>
+        )
+
     }
 }
 
