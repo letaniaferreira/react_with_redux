d1ac9d13a90c67cfb5b112834217ade7a08da1d5
Letania Ferreira <letania@rollbar.com>
2019-09-06 22:53:11 -0700

    Change collor on ApprovalCard

--- blog_posts/components/src/ApprovalCard.js
+++ blog_posts/components/src/ApprovalCard.js
@@ -7,8 +7,8 @@ const ApprovalCard = props => {
 			<div className="text">{props.children}</div>
 			<div className="extra content">
 				<div className="ui two buttons">
-				<div className="ui basic green button">Approve</div>
-				<div className="ui basic red button">Reject</div>
+				<div className="ui basic blue button">Approve</div>
+				<div className="ui basic orange button">Reject</div>
 				</div>
 			</div>
 		</div>
