5b8c04222fcb50e15cfc2ef7aee39f34eff8397f
Letania Ferreira <letania@rollbar.com>
2019-11-10 19:01:34 -0800

    Add preventDefault to form onSubmit

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -4,10 +4,14 @@ class SearchBar extends React.Component {
 
     state = { term: ''};
 
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render  () {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                     <input 
