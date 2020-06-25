28818d30ba66489a8d180ade3d02d82dffe0e878
Letania Ferreira <letania@rollbar.com>
2020-06-24 18:35:19 -0700

    Add formating to button using semantic ui classNames

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -20,7 +20,7 @@ class SongList extends React.Component {
     }
 
     render() {
-        return <div>{this.renderList()}</div>;
+        return <div className="ui divided list">{this.renderList()}</div>;
     }
 
 }
