dde1dbc47323cc177e789c56c911125ea0c3ffc0
Letania Ferreira <letania@rollbar.com>
2020-07-11 16:31:17 -0700

    Add a promisse using async await syntax

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,15 +5,15 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-	onSearchSubmit(term) {
-		axios.get('https://api.unsplash.com/search/photos', {
+	async onSearchSubmit(term) {
+		const response = await axios.get('https://api.unsplash.com/search/photos', {
 			params: { query: term },
 			headers: {
 				Authorization: 'Client-ID 46267918263db9507b919b1dce011aadb920ba2b66f70a7ab9a194f63bdf5f38'
 			}
-		}).then(response => {
-			console.log(response.data.results);
 		});
+		
+		console.log(response.data.results);
 	}
 
 	render () {
