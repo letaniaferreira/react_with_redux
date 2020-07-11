ffdf22a7dcc4093baea033a9cc8b0d841fb4662b
Letania Ferreira <letania@rollbar.com>
2020-07-11 16:17:57 -0700

    Add API call via axios to App

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
 
 	onSearchSubmit(term) {
-		console.log(term)
+		axios.get('https://api.unsplash.com/search/photos', {
+			params: { query: term },
+			headers: {
+				Authorization: 'Client-ID 46267918263db9507b919b1dce011aadb920ba2b66f70a7ab9a194f63bdf5f38'
+			}
+		});
 	}
 
 	render () {
