4f4b2479a5b16bd920b30f4c32573867a6efd045
Letania Ferreira <letania@rollbar.com>
2020-07-11 16:26:59 -0700

    Add a .then promisse

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -11,6 +11,8 @@ class App extends React.Component {
 			headers: {
 				Authorization: 'Client-ID 46267918263db9507b919b1dce011aadb920ba2b66f70a7ab9a194f63bdf5f38'
 			}
+		}).then(response => {
+			console.log(response.data.results);
 		});
 	}
 
