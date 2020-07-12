79c3e6c5d017a555eea4a932ab85fca787093900
Letania Ferreira <letania@rollbar.com>
2020-07-11 23:17:43 -0700

    Deestructure image props in ImageCard component

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class ImageCard extends React.Component {
     render() {
+        const { alt_description, urls} = this.props.image;
+
         return (
             <div>
                 <img 
-                alt={this.props.image.alt_description}
-                src={this.props.image.urls.regular} 
+                alt={alt_description}
+                src={urls.regular} 
                 />
             </div>
         );
