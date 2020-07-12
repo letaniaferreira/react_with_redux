5605ac48f4ed9721ff8003dec7764a39b8553aa4
Letania Ferreira <letania@rollbar.com>
2020-07-11 22:41:15 -0700

    Add alt_description to images

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImageList = props => {
     const carimages = props.carimages.map((image) => {
-        return <img  key={image.id} src={image.urls.regular} />;
+        return <img alt={image.alt_description} key={image.id} src={image.urls.regular} />;
     })
     return <div>{carimages}</div>;
 };
