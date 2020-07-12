83f225c21d5f86ddcb9bad94d7478c5c2ef26739
Letania Ferreira <letania@rollbar.com>
2020-07-11 23:27:34 -0700

    Create React Reference in IamgeCard constructor

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
     render() {
         const { alt_description, urls} = this.props.image;
 
