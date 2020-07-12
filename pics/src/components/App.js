c24b4b1a358fa204e24a30c26e44764f5cafa9a5
Letania Ferreira <letania@rollbar.com>
2020-07-11 20:36:50 -0700

    Set state of images and reformat async function

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,21 +5,24 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-	async onSearchSubmit(term) {
+	state = { images: [] };
+
+	onSearchSubmit = async (term) => {
 		const response = await axios.get('https://api.unsplash.com/search/photos', {
 			params: { query: term },
 			headers: {
 				Authorization: 'Client-ID 46267918263db9507b919b1dce011aadb920ba2b66f70a7ab9a194f63bdf5f38'
 			}
 		});
-		
-		console.log(response.data.results);
+
+		this.setState({ images: response.data.results });
 	}
 
 	render () {
 		return (
 			<div className="ui container" style={{ marginTop: '10px'}}>
 				<SearchBar onSubmitingForm={this.onSearchSubmit}/>
+				Found: {this.state.images.length} images
 			</div>
 		);
 	}
