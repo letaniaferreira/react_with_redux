2e3e45c70a5f2a2a0c28b538ca55f97c113c83c2
Letania Ferreira <letania@rollbar.com>
2020-07-11 22:57:05 -0700

    Implement CSS grid system for showing images

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,11 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = props => {
     const carimages = props.carimages.map(({ alt_description, id, urls }) => {
         return <img alt={alt_description} key={id} src={urls.regular} />;
     })
-    return <div>{carimages}</div>;
+    return <div className='image-list'>{carimages}</div>;
 };
 
 export default ImageList;
