430139ca7a6a75ddd7912017b3bbca4794ec6b2a
Letania Ferreira <letania@rollbar.com>
2020-07-11 22:45:08 -0700

    Reformat by deestructuring image props

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const ImageList = props => {
-    const carimages = props.carimages.map((image) => {
-        return <img alt={image.alt_description} key={image.id} src={image.urls.regular} />;
+    const carimages = props.carimages.map(({ alt_description, id, urls }) => {
+        return <img alt={alt_description} key={id} src={urls.regular} />;
     })
     return <div>{carimages}</div>;
 };
