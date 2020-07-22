4179813a6b2069ec2990cc69e9d97def2497b519
Letania Ferreira <letania@rollbar.com>
2020-07-21 19:55:26 -0700

    Control to searcbar input

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -3,13 +3,22 @@ import React from "react";
 class SearchBar extends React.Component {
     state = {searchterm: ''};
 
+    onInputChange = (event) => {
+        this.setState({searchterm: event.target.value });
+    };
+
+
     render(){
         return(
             <div className="search-bar ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Video Search</label>
-                        <input type="text" value={this.state.searchterm} />
+                        <input
+                            type="text"
+                            value={this.state.searchterm}
+                            onChange={this.onInputChange}
+                       />
                     </div>
                 </form>
             </div>
