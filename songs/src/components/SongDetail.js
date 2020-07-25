de02dbd20df830134c3ffea1ca25137d047242bb
Letania Ferreira <letania@rollbar.com>
2020-07-24 19:24:19 -0700

    Add connect to SongDetail

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,10 +1,14 @@
 import React from "react";
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
+import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
 
-const = SongDetail => () {
+const = SongDetail => (props) {
+    console.log(props)
     return <div>Song Detail</div>
 };
 
+const = mapStateToProps => (state) {
+    return { song: state.SelectedSongReducer }
 }
 
-export default SongDetail;
+export default connect(mapStateToProps) (SongDetail);
